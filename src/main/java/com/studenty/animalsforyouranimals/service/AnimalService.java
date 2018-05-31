f6b08d89d7c6d95e1ef650833d51338f495a04a1
Sławomir Wójcik <swojcik1996@gmail.com>
2018-05-31 19:31:26 +0200

    hibernate search conf, ad page

--- src/main/java/com/studenty/animalsforyouranimals/service/AnimalService.java
+++ src/main/java/com/studenty/animalsforyouranimals/service/AnimalService.java
@@ -3,9 +3,6 @@ package com.studenty.animalsforyouranimals.service;
 /**
  * Created by Sławomir Wójcik on 2018-05-31.
  */
-public class AnimalServiceImpl implements AnimalService {
-    @Override
-    public void addAnimals() {
-
-    }
+public interface AnimalService {
+    void addAnimals();
 }
---
+++ src/main/java/com/studenty/animalsforyouranimals/service/HibernateSearchService.java
@@ -0,0 +1,64 @@
+package com.studenty.animalsforyouranimals.service;
+
+import com.studenty.animalsforyouranimals.model.Animal;
+import org.apache.lucene.search.Query;
+import org.hibernate.search.jpa.FullTextEntityManager;
+import org.hibernate.search.jpa.Search;
+import org.hibernate.search.query.dsl.QueryBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import java.util.List;
+/**
+ * Created by Sławomir Wójcik on 2018-05-31.
+ */
+public class HibernateSearchService {
+    @Autowired
+    private final EntityManager centityManager;
+
+
+    @Autowired
+    public HibernateSearchService(EntityManager entityManager) {
+        super();
+        this.centityManager = entityManager;
+    }
+
+    public void initializeHibernateSearch() {
+
+        try {
+            FullTextEntityManager fullTextEntityManager = Search.getFullTextEntityManager(centityManager);
+            fullTextEntityManager.createIndexer().startAndWait();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Transactional
+    public List<Animal> fuzzySearch(String searchTerm) {
+
+        FullTextEntityManager fullTextEntityManager = Search.getFullTextEntityManager(centityManager);
+        QueryBuilder qb = fullTextEntityManager.getSearchFactory().buildQueryBuilder().forEntity(Animal.class).get();
+        Query luceneQuery = qb.keyword().fuzzy().withEditDistanceUpTo(1).withPrefixLength(1).onFields("name")
+                .matching(searchTerm).createQuery();
+
+        javax.persistence.Query jpaQuery = fullTextEntityManager.createFullTextQuery(luceneQuery, Animal.class);
+
+        // execute search
+
+        List<Animal> AnimalResult = null;
+        try {
+            AnimalResult = jpaQuery.getResultList();
+        } catch (NoResultException nre) {
+            ;// do nothing
+
+        }
+
+        return AnimalResult;
+
+
+
+    }
+}
